7a05bb3a0960d3310d6c2faf4e284070f0fc877c
Mauvebar <mauvebar@student.21-school.ru>
2024-09-05 08:17:30 +0300

    commit

---
+++ src/main/java/org/example/MyArrayList.java
@@ -0,0 +1,163 @@
+package org.example;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+/***
+ * Generic class for handling arrays of various types
+ * @param <T>
+ */
+public class MyArrayList<T extends Object> implements Comparator<T> {
+    Object[] arr;
+    int size=0;
+    public MyArrayList(){
+        arr=new Object[10];
+    }
+    public void add(T ob){
+        if(size==arr.length)
+            moreCapacity();
+        arr[size]=ob;
+        size+=1;
+    }
+    public void add(int index,T ob){
+        if(index<0)
+            throw new IndexOutOfBoundsException("Out of boundaries");
+        while(index>=arr.length)
+            moreCapacity();
+        if(arr[index]!=null)
+            throw new RuntimeException("Already occupied");
+        arr[index]=ob;
+        size+=1;
+    }
+    public Object replace(int index,T ob){
+        if(index<0 || index>=arr.length)
+            throw new IndexOutOfBoundsException("Out of boundaries");
+        Object prev=arr[index];
+        arr[index]=ob;
+        return prev;
+    }
+    void moreCapacity(){
+        try {
+            arr = Arrays.copyOf(arr, arr.length * 2);
+        }catch(Exception e){
+            System.out.println("Unexpected Error");
+        }
+    }
+    public Object get(int index){
+        if(index<0 || index>=arr.length)
+            throw new IndexOutOfBoundsException("Out of boundaries");
+        return arr[index];
+    }
+    public Object remove(int index){
+        if(index<0 || index>=arr.length)
+            throw new IndexOutOfBoundsException("Out of boundaries");
+        Object ob=arr[index];
+        if(arr[index]!=null)
+            size-=1;
+        arr[index] = null;
+        return ob;
+    }
+    public int remove(T ob){
+        for(int i=0; i<arr.length; ++i)
+            if(arr[i]==ob) {
+                arr[i]=null;
+                size-=1;
+                return i;
+            }
+        return -1;
+    }
+    public void clean(){
+        for(int i=0; i<arr.length; ++i)
+            arr[i]=null;
+        size=0;
+    }
+    @Override
+    public int compare(T ob1, T ob2) {
+        if(ob1 instanceof Byte || ob1 instanceof Short || ob1 instanceof Integer || ob1 instanceof Character) {
+            if ((Integer) ob1 > (Integer) ob2)
+                return 1;
+            else if ((Integer) ob1 < (Integer) ob2)
+                return -1;
+            else return 0;
+        }
+        else if(ob1 instanceof Long) {
+            if ((Long) ob1 > (Long) ob2)
+                return 1;
+            else if ((Long) ob1 < (Long) ob2)
+                return -1;
+            else return 0;
+        }
+        else if(ob1 instanceof Float || ob1 instanceof Double){
+            if ((Double) ob1 > (Double) ob2)
+                return 1;
+            else if ((Double) ob1 < (Double) ob2)
+                return -1;
+            else return 0;
+        }
+        else if(ob1 instanceof String)
+            return ((String) ob1).compareTo((String) ob2);
+        else{
+            if(ob1.hashCode()>ob2.hashCode())
+                return 1;
+            if(ob1.hashCode()<ob2.hashCode())
+                return -1;
+            else return 0;
+        }
+    }
+    public void sort(){
+        for(int i=0; i<arr.length-1; ++i)
+            if(arr[i]==null && arr[i+1]!=null){
+                arr[i]=arr[i+1];
+                arr[i+1]=null;
+                i=-1;
+            }
+        Object ob;
+        for(int i=0; arr[i+1]!=null; ++i)
+            if(compare((T)arr[i],(T)arr[i+1])==1){
+                ob=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=ob;
+                i=-1;
+            }
+    }
+    public void quicksort(){
+        int i, last;
+        for(i=0; arr[i]==null && i<arr.length; ++i);
+        if(i==arr.length)
+            return;
+        for(last=-1; i<arr.length; ++i)
+            if(arr[i]!=null){
+                last+=1;
+                if(i-last>0){
+                    arr[last]=arr[i];
+                    arr[i]=null;
+                }
+            }
+        do {
+            Object pivot=arr[last];
+            for (i = 0; i < last; ++i)
+                if (compare((T) arr[i], (T) pivot)==1) {
+                    arr[last]=arr[i];
+                    arr[i]=pivot;
+                    pivot=arr[last];
+                }
+        }while(--last>0);
+    }
+    public void showAll(){
+        for(int i=0; i<arr.length; ++i)
+            if(arr[i]!=null)
+                System.out.println(arr[i]);
+    }
+    public int capacity(){
+        return arr.length;
+    }
+    public int size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        for(int i=0; i<arr.length; ++i)
+            if(arr[i]!=null)
+                return false;
+        return true;
+    }
+}
---
+++ src/test/java/org/example/MyArrayListTest.java
@@ -0,0 +1,91 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyArrayListTest {
+    MyArrayList<Integer> num;
+    MyArrayList<Character> ch;
+    MyArrayList<String> str;
+    @BeforeEach
+    void setUp() {
+        num=new MyArrayList<>();
+        ch=new MyArrayList<>();
+        str=new MyArrayList<>();
+    }
+
+    @Test
+    void replace() {
+        num.add(7);
+        assertEquals(7,num.get(0));
+        num.add(10,3);
+        assertEquals(7,num.replace(0,5));
+        assertEquals(5,num.get(0));
+    }
+
+    @Test
+    void get() {
+        num.add(7);
+        assertEquals(7,num.get(0));
+        num.add(10,3);
+        assertEquals(3,num.get(10));
+        str.add("cat");
+        assertEquals("cat",str.get(0));
+        str.add(2,"kot");
+        assertEquals("kot",str.get(2));
+        ch.add('x');
+        assertEquals('x',ch.get(0));
+        ch.add(1,'a');
+        assertEquals('a',ch.get(1));
+    }
+
+    @Test
+    void remove() {
+        num.add(5,10);
+        assertEquals(10,num.get(5));
+        assertEquals(null,num.get(0));
+        str.add("dog"); str.add(5,"cat");
+        assertEquals(5,str.remove("cat"));
+        assertEquals(0,str.remove("dog"));
+        assertEquals(-1,str.remove("horse"));
+    }
+
+    @Test
+    void capacity() {
+        assertEquals(10,num.capacity());
+        num.add(10,0);
+        assertEquals(20,num.capacity());
+    }
+
+    @Test
+    void size() {
+        assertEquals(0,ch.size());
+        ch.add(10,'c');
+        assertEquals(1,ch.size());
+        ch.add('t');
+        assertEquals(2,ch.size());
+        ch.remove(10);
+        assertEquals(1,ch.size());
+        ch.remove(9);
+        assertEquals(1,ch.size());
+    }
+
+    @Test
+    void isEmpty() {
+        assertEquals(true,str.isEmpty());
+        str.add("cat");
+        assertEquals(false,str.isEmpty());
+        str.remove(0);
+        assertEquals(true,str.isEmpty());
+        for(int i=0; i<1000; ++i)
+            num.add(i);
+        for(int i=0; i<1000; ++i)
+            assertEquals(i,num.get(i));
+        num.clean();
+        assertEquals(true,num.isEmpty());
+        for(int i=0; i<num.capacity(); ++i)
+            assertEquals(null,num.get(i));
+    }
+}
